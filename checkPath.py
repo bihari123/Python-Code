8596a76d6a9c9a1ca2d1fb4842a00be56e32b37f
TARUN THAKUR <bihari123@users.noreply.github.com>
2021-12-25 20:04:59 +0530

    Add files via upload

---
+++ checkPath.py
@@ -0,0 +1,9 @@
+import os  
+path="$HOME/.sail/log/process_list.log"  
+if os.path.isdir(path):  
+    print("\nIt is a directory")  
+elif os.path.isfile(path):  
+    print("\nIt is a normal file")  
+else:  
+    print("It is a special file (socket, FIFO, device file)" )
+print()
